b087ee32f10a9f1237033652fc4b0d60b025c0f4
Gasbylei <gasbylei@foxmail.com>
2019-03-25 11:25:09 +0800

    add topicfiler and pagination

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,11 +6,9 @@
       <div class="panel panel-default">
         <div class="panel-heading">
           <ul class="list-inline topic-filter">
-            <li><a href="/topics?filter=default" class="active">活跃</a></li>
-            <li><a href="/topics?filter=excellent">精华</a></li>
-            <li><a href="/topics?filter=vote">投票</a></li>
-            <li><a href="/topics?filter=recent">最近</a></li>
-            <li><a href="/topics?filter=noreply">零回复</a></li>
+            <li v-for="item in filters" :key="item.id">
+              <router-link v-title="item.title" :class="{ active: filter === item.filter }" :to="`/topics?filter=${item.filter}`">{{ item.name }}</router-link>
+            </li>
           </ul>
           <div class="clearfix"></div>
         </div>
@@ -39,6 +37,10 @@
           </ul>
         </div>
 
+        <div class="panel-footer text-right remove-padding-horizontal pager-footer">
+          <Pagination :currentPage="currentPage" :total="total" :pageSize="pageSize" :onPageChange="changePage" />
+        </div>
+
       </div>
     </div>
 
@@ -52,9 +54,20 @@ export default {
   name: 'Home',
   data() {
     return {
-      msg: '', // 消息
-      msgType: '', // 消息类型
-      msgShow: false // 是否显示消息，默认不显示
+      msg: '',
+      msgType: '',
+      msgShow: false,
+      articles: [], // 文章列表
+      filter: 'default', // 默认过滤方式
+      filters: [ // 过滤方式列表
+        { filter: 'default', name: '活跃', title: '最后回复排序'},
+        { filter: 'excellent', name: '精华', title: '只看加精的话题'},
+        { filter: 'vote', name: '投票', title: '点赞数排序'},
+        { filter: 'recent', name: '最近', title: '发布时间排序'},
+        { filter: 'noreply', name: '零回复', title: '无人问津的话题'}
+      ],
+      total: 0, // 文章总数
+      pageSize: 10, // 每页条数
     }
   },
   beforeRouteEnter(to, from, next) {
@@ -67,15 +80,16 @@ export default {
           case 'Register':
             vm.showMsg('注册成功')
             break
-          // 已登录时，从登录页面跳转过来
           case 'Login':
-            // 显示登录成功
             vm.showMsg('登录成功')
             break
         }
       } else if (logout) {
         vm.showMsg('操作成功')
       }
+
+      // 确认渲染该组件的对应路由时，设置相关数据
+      vm.setDataByFilter(to.query.filter)
     })
   },
   computed: {
@@ -83,8 +97,9 @@ export default {
       'auth',
       'user'
     ]),
-    articles() {
-      return this.$store.getters.computedArticles
+    // 当前页，从查询参数 page 返回
+    currentPage() {
+      return parseInt(this.$route.query.page) || 1
     }
   },
   watch: {
@@ -92,6 +107,10 @@ export default {
       if (!value) {
         this.showMsg('操作成功')
       }
+    },
+    // 监听 '$route'，在查询参数变化后，设置相关数据
+    '$route'(to) {
+      this.setDataByFilter(to.query.filter)
     }
   },
   methods: {
@@ -99,6 +118,27 @@ export default {
       this.msg = msg
       this.msgType = type
       this.msgShow = true
+    },
+    // 设置相关数据
+    setDataByFilter(filter = 'default') {
+      // 每页条数
+      const pageSize = this.pageSize
+      // 当前页
+      const currentPage = this.currentPage
+      // 过滤后的所有文章
+      const allArticles = this.$store.getters.getArticlesByFilter(filter)
+
+      this.filter = filter
+      // 文章总数
+      this.total = allArticles.length
+      // 当前页的文章
+      this.articles = allArticles.slice(pageSize * (currentPage - 1), pageSize * currentPage)
+    },
+    // 回调，组件的当前页改变时调用
+    changePage(page) {
+      // 在查询参数中混入 page，并跳转到该地址
+      // 混入部分等价于 Object.assign({}, this.$route.query, { page: page })
+      this.$router.push({ query: { ...this.$route.query, page } })
     }
   }
 }
